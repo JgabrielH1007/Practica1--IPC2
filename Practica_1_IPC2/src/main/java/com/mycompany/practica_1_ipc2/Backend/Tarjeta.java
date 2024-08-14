b48ecbf101913132e5d660d047f4f8268d1980c8
GabrielH <fredyherrera202130478@cunoc.edu.gt>
2024-08-13 23:32:20 -0600

    Inicio practica

---
+++ Practica_1_IPC2/src/main/java/com/mycompany/practica_1_ipc2/Backend/Tarjeta.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.practica_1_ipc2.Backend;
+
+/**
+ *
+ * @author gabrielh
+ */
+public class Tarjeta {
+    
+}
